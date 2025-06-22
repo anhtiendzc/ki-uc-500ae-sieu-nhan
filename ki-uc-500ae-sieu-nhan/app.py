02bec58663f57d6482285cfe0f2e9fbdc1c6edae
anhtiendzc <tinv65985@gmail.com>
2025-06-22 14:02:42 +0700

    Update app.py

--- ki-uc-500ae-sieu-nhan/app.py
+++ ki-uc-500ae-sieu-nhan/app.py
@@ -5,9 +5,11 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 100MB
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    uploaded_file = None
     if request.method == 'POST':
         if 'file' not in request.files:
             return 'No file part'
@@ -18,9 +20,12 @@ def index():
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
-            return redirect(url_for('index'))
+            uploaded_file = filename
+
     files = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('index.html', files=files)
+    files.sort(reverse=True)  # sắp xếp mới nhất lên đầu
+    return render_template('index.html', files=files, uploaded_file=uploaded_file)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
